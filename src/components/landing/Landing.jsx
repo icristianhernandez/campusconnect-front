54102aaa5f2bd2b9055e238ee3dff17086f5c538
Migue-709 <orozcoml709@gmail.com>
2025-03-29 02:41:16 -0400

    login page upgrade

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -1,22 +1,21 @@
 import './Landing.css'
 import Login from './login/Login'
-import usmBackground from './assets/usm-background.jpg'
-import { useContext } from 'react';
-import { MyContext } from '../../context/context';
+import bannerBackground from './assets/Banner-Web-USM-2.jpg'
 
 function Landing(){
 
     /*BackGround*/
     const backgroundStyle = {
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${usmBackground})`,
+        backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(119, 119, 119, 0.7)), url(${bannerBackground})`, // Gray gradient overlay
         backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat'
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center' // Ensure the image is centered
     }
 
     return (
         <div style={backgroundStyle} className="Landing">
             <div className='logo-container'>
-                <img src={`Logo-completo.png`} alt="Logo" className='landing-logo-completo'/>
+                <img src={`Logo-completo-blanco.png`} alt="Logo" className='landing-logo-completo'/>
             </div>
             <Login/>
         </div>
